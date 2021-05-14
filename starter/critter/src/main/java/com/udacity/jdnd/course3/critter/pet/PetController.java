b32d097bed26416ec6b6e7f2532c953975cb86dc
dcsiona@gmail.com <dcsiona@gmail.com>
2021-05-14 05:44:17 -0400

    Committing to my PetController.java

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/PetController.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/PetController.java
@@ -1,7 +1,12 @@
 package com.udacity.jdnd.course3.critter.pet;
 
 import org.springframework.web.bind.annotation.*;
+import com.udacity.jdnd.course3.critter.PetDTO;
+import com.udacity.jdnd.course3.critter.Entity.Pet;
+import com.udacity.jdnd.course3.critter.service.PetService;
+import org.springframework.beans.BeanUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,23 +16,51 @@ import java.util.List;
 @RequestMapping("/pet")
 public class PetController {
 
+    @Autowired
+    private PetService petService;
+
+
+    private pet convertPetToPetDTO(PetDTO petDTO) {
+        Pet pet = new pet();
+        BeanUtils.copyProperties(PetDTO, pet);
+        return pet;
+    }
+
+    private PetDTO convertPetToPetDTO(Pet pet) {
+        PetDTO petDTO = new PetDTO();
+        BeanUtils.copyProperties(pet, petDTO);
+        return petDTO;
+    }
+
+
     @PostMapping
     public PetDTO savePet(@RequestBody PetDTO petDTO) {
-        throw new UnsupportedOperationException();
+        Pet pet = convertPetToPetDTO(petDTO);
+        Pet savedPet = petService.savePet(pet);
+        return convertPetToPetDTO(Pet pet);
+
     }
 
     @GetMapping("/{petId}")
     public PetDTO getPet(@PathVariable long petId) {
-        throw new UnsupportedOperationException();
+        Pet pet = petService.findPetById(petId);
+        return convertPetToPetDTO(Pet pet);
     }
 
     @GetMapping
     public List<PetDTO> getPets(){
+
         throw new UnsupportedOperationException();
     }
 
     @GetMapping("/owner/{ownerId}")
     public List<PetDTO> getPetsByOwner(@PathVariable long ownerId) {
-        throw new UnsupportedOperationException();
+        List<Pet> pet = petService.findPetsByOwner(OwnerId);
+        List<PetDTO> petDTO = new ArrayList<PetDTO>();
+
+        for (Pet pet: pet) {
+            petDTO.add(convertPetToPetDTO(pet));
+        }
+        return petDTO;
     }
 }
