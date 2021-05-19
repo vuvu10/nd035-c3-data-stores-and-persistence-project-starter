91ddb67dbf84fd63ef931e3ca6f4523207829501
dcsiona@gmail.com <dcsiona@gmail.com>
2021-05-19 06:18:24 -0400

    Committing to my PetController.java

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/employeeRepository.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/employeeRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 @Repository
 public interface employeeRepository extends JpaRepository<Employee, Long>{
-    List<Employee>();
+    List<Employee>findAllByDaysAvailable(DaysOfWeek daysOfWeek);
 }
