2ff86c623c8c666660627fe1a937cb31583abc46
dcsiona@gmail.com <dcsiona@gmail.com>
2021-05-19 04:35:10 -0400

    Committing to my PetController.java

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/scheduleRepository.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/scheduleRepository.java
@@ -1,6 +1,6 @@
 package com.udacity.jdnd.course3.critter.repository;
 
-import com.udacity.jdnd.course3.critter.entity.Schedule;
+import com.udacity.jdnd.course3.critter.entity.schedule;
 import com.udacity.jdnd.course3.critter.entity.Pet;
 import com.udacity.jdnd.course3.critter.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,9 +9,9 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface scheduleRepository extends JpaRepository<Schedule, long>{
-    List<Schedule> getAllByPets(Pet pet);
-    List<Schedule> getAllByPets(Pet pet);
-    List<Schedule> getAllByPets(Pet pet);
+public interface ScheduleRepository extends JpaRepository<Schedule, long>{
+    List<Schedule> findAllPets(Pet pet);
+    List<Schedule> findAllEmployees(Employee employee);
+    List<Schedule> findAllPetsIn(List<Pet> pet);
 
 }
